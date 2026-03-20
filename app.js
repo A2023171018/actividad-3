0206808f878c526bd1fa7cdda0bfb4591adc18f8
Angel Aguilar <156367469+A2023171018@users.noreply.github.com>
2026-03-20 00:40:28 -0600

    Update app.js

--- app.js
+++ app.js
@@ -2,12 +2,45 @@ var express = require('express')
 var path = require('path')
 var cookieParser = require('cookie-parser')
 var logger = require('morgan')
+var client = require('prom-client')
 
 var indexRouter = require('./routes/index')
 var usersRouter = require('./routes/users')
 var itemsRouter = require('./routes/items')
 
 var app = express()
+
+// --- Prometheus ---
+client.collectDefaultMetrics()
+
+const httpRequestCounter = new client.Counter({
+  name: 'http_requests_total',
+  help: 'Total de peticiones HTTP procesadas',
+  labelNames: ['metodo', 'ruta', 'estado_http'],
+})
+
+const activeUsersGauge = new client.Gauge({
+  name: 'active_users_current',
+  help: 'Número actual de usuarios activos simulados',
+})
+
+// Simula usuarios activos aleatorios cada 5 segundos
+setInterval(() => {
+  activeUsersGauge.set(Math.floor(Math.random() * 50))
+}, 5000)
+
+// Middleware para contar peticiones
+app.use((req, res, next) => {
+  res.on('finish', () => {
+    httpRequestCounter.inc({
+      metodo: req.method,
+      ruta: req.path,
+      estado_http: String(res.statusCode),
+    })
+  })
+  next()
+})
+// --- Fin Prometheus ---
 
 app.use(logger('dev'))
 app.use(express.json())
@@ -18,5 +51,11 @@ app.use(express.static(path.join(__dirname, 'public')))
 app.use('/', indexRouter)
 app.use('/users', usersRouter)
 app.use('/items', itemsRouter)
+
+// Endpoint que Prometheus leerá
+app.get('/metrics', async (req, res) => {
+  res.set('Content-Type', client.register.contentType)
+  res.send(await client.register.metrics())
+})
 
 module.exports = app
